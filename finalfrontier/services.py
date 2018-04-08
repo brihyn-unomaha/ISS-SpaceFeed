43a37192b15d07d53207d7025a6eb3ec9f60ac85
brihyn-unomaha <35381257+brihyn-unomaha@users.noreply.github.com>
2018-04-08 12:10:48 -0500

    Adding some comments to py files

--- finalfrontier/services.py
+++ finalfrontier/services.py
@@ -1,14 +1,11 @@
 import requests
 import json
 import urllib.request
-#from .models import Space
 
-# Function get_iss_location gets the current location of the ISS, and adds to three attributes, locaiton (combined lat/lon), lat, and lon
+# Function get_iss_location gets the current location of the ISS as a comobined lat/lon json
 def get_iss_location():
     url = f'http://api.open-notify.org/iss-now.json'
     response = urllib.request.urlopen(url)
     result= json.loads(response.read())
     location = result['iss_position']
-    #Space.lat=Space.location['latitude']
-    #Space.lon=Space.location['longitude']
     return(location)
--- finalfrontier/views.py
+++ finalfrontier/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
 from .models import Space
 from.services import get_iss_location
 
 
 def home(request):
-    #space = get_object_or_404(Space)
+    #We're combining all of the data into a class named Space as attributes.
+
+    #ISS Data Retrieval
+    #Call the Services.py file function to get the various space data
     Space.location = get_iss_location()
+    # Separate out the individual latitude and longitude
     Space.lat=Space.location['latitude']
     Space.lon = Space.location['longitude']
+
+    #NASA RSS Feed retrieval for Project Status
+
+
+    # Send the Space class to the html page
     return render(request, 'finalfrontier/home.html', {'Space': Space})
 
